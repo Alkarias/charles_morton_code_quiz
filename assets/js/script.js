03eb4155c105a76e8242fb4ac7e8d38b77ae415c
Alkarias <alkarias29@gmail.com>
2022-06-26 17:08:57 -0400

    added question shuffling and answer detection

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
-var containerEl = document.querySelector('#container'); //grabs the section element
-var h1El = document.getElementById('header'); //grabs the h1 element
-var description = document.getElementById('description'); //grabs the p element
-var btnStart = document.getElementById('startBtn'); //grabs the starting button element
+//grabbing all of the elements of the start screen
+var containerEl = document.querySelector('#container'); 
+var h1El = document.getElementById('header');
+var description = document.getElementById('description');
+var btnStart = document.getElementById('startBtn');
 
 //this is the setup for the questions screen
 var ansForm = document.createElement("form");
@@ -11,15 +12,21 @@ var btn2 = document.createElement("button");
 var btn3 = document.createElement("button");
 var btn4 = document.createElement("button");
 
+//clones the question list so that the original is unaltered by .splice()
 var listClone = questionList.questions;
-console.log(listClone);
+
+//variable used to store the score from the current attempt
+var score = 0;
 
 //when the start button is pressed, change the display to show
 //the multiple choice questions
 btnStart.addEventListener('click', function() {
+    //unrenders the unnecessary objects from the start screen
     description.setAttribute('style', 'display:none');
     btnStart.setAttribute('style', 'display:none');
+    //picks the question that is displayed
     nextQuestion();
+    //appends the needed elements into the display
     containerEl.appendChild(ansForm);
     ansForm.appendChild(btn1);
     ansForm.appendChild(btn2);
@@ -27,22 +34,75 @@ btnStart.addEventListener('click', function() {
     ansForm.appendChild(btn4);
 });
 
-// btn1.addEventListener('click', nextQuestion());
-// btn2.addEventListener('click', nextQuestion());
-// btn3.addEventListener('click', nextQuestion());
-// btn4.addEventListener('click', nextQuestion());
+btn1.addEventListener('click', buttonClick);
+btn2.addEventListener('click', buttonClick);
+btn3.addEventListener('click', buttonClick);
+btn4.addEventListener('click', buttonClick);
+
+function buttonClick(event) {
+    event.preventDefault();
+    checkCorrect(event.target);
+    nextQuestion();
+}
 
 function nextQuestion() {
+    //chooses a random question from the cloned question list
     // var questionIndex = Math.floor(Math.random() * listClone.length);
     var questionIndex = 0;
-    var questionNum = listClone.splice(questionIndex,1);
-    
+    //removes the chosen question from the list so it cannot be picked again
+    //and allows reference to the chosen question
+    var questionNum = listClone.splice(questionIndex,1); //returns an array with a length of 1
+    //applies the answers to a random button
+    var answers = shuffle(questionNum[0]);
+
+    //updates the text to display the new question
     h1El.textContent = questionNum[0].question;
-    btn1.textContent = "answer";
-    btn2.textContent = "answer";
-    btn3.textContent = "answer";
-    btn4.textContent = "answer";
-    
-    console.log(questionNum);
+    btn1.textContent = answers[0];
+    btn2.textContent = answers[1];
+    btn3.textContent = answers[2];
+    btn4.textContent = answers[3];
+}
+
+function shuffle(item) { //shuffles the answers so they are placed onto random buttons
+    var answersTemp = item.answers;
+    var answers = [];
+    var answerSet = false;
+    for (var i = 0; i < 4; i++) {
+        var index = Math.floor(Math.random() * answersTemp.length);
+        answers.push(answersTemp[index]);
+        answersTemp.splice(index, 1);
+        if (index == 0 && !answerSet) { //tells which button is holding the correct answer
+            btnReset(i);
+            answerSet = true;
+        }
+    }
+    return answers;
+}
+
+function btnReset(index) {
+    //resets which button is correct
+    btn1.setAttribute('data-bool', 'false');
+    btn2.setAttribute('data-bool', 'false');
+    btn3.setAttribute('data-bool', 'false');
+    btn4.setAttribute('data-bool', 'false');
+
+    //this is temporary for matching correct answer to the data attribute
+    btn1.setAttribute('style', 'background-color:#764faf');
+    btn2.setAttribute('style', 'background-color:#764faf');
+    btn3.setAttribute('style', 'background-color:#764faf');
+    btn4.setAttribute('style', 'background-color:#764faf');
+
+    //communicates to the button that it is holding the correct answer
+    window["btn" + (index+1)].setAttribute('data-bool', 'true');
+    window["btn" + (index+1)].setAttribute('style', 'background-color:green');
+}
+
+function checkCorrect(target) {
+    if (target.getAttribute('data-bool') == 'true') {
+        score++;
+    } else {
+        // time -= 5;
+    }
+    console.log(score);
 }
 
