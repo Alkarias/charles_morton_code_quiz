68d6f58169e7c118898e7e4d2ced6fec8fcbc99f
Alkarias <alkarias29@gmail.com>
2022-06-26 22:01:25 -0400

    finalized list of questions

--- assets/js/questionList.js
+++ assets/js/questionList.js
@@ -9,96 +9,96 @@ const questionList = {
         answers:["0","1","a","first"]
     },
     { // #3
-        question: "", 
-        answers: ["","","",""]
+        question: "What is the output of the window.prompt() method?", 
+        answers: ["string","number","boolean","There is no output"]
     },
     { // #4
-        question: "",
-        answers: ["","","",""]
+        question: "What is the output of the window.confirm() method?",
+        answers: ["boolean","string","number","There is no output"]
     },
     { // #5
-        question: "",
-        answers: ["","","",""]
+        question: "What is the output of the window.alert() method?",
+        answers: ["There is no output","string","number","boolean"]
     },
     { // #6
-        question: "",
-        answers: ["","","",""]
+        question: "Which of the following is NOT a javascript datatype?",
+        answers: ["letter","string","number","object"]
     },
     { // #7
-        question: "",
-        answers: ["","","",""]
+        question: "Which method should be used to add a value to the end of an array?",
+        answers: [".push()",".pop()",".shift()",".unshift()"]
     },
     { // #8
-        question: "",
-        answers: ["","","",""]
+        question: "Which method should be used to remove a value from the beginning of an array?",
+        answers: [".unshift()",".shift()",".push()",".pop()"]
     },
     { // #9
-        question: "",
-        answers: ["","","",""]
+        question: "console.log(typeof '0'); will log what value?",
+        answers: ["string","number","NaN","No value will be logged"]
     },
     { // #10
-        question: "",
-        answers: ["","","",""]
+        question: "console.log(typeof true) will log what value?",
+        answers: ["No value will be logged","string","number","boolean"]
     },
     { // #11
-        question: "",
-        answers: ["","","",""]
+        question: "What code structure is used to iterate through lines of code mulitple times?",
+        answers: ["loop","repeat","cycle","iterator"]
     },
     { // #12
-        question: "",
-        answers: ["","","",""]
+        question: "What symbol is used to create an object variable?",
+        answers: ["{}","[]","()","<>"]
     },
     { // #13
-        question: "",
-        answers: ["","","",""]
+        question: "What symbol is used to create an array variable?",
+        answers: ["[]","{}","()","<>"]
     },
     { // #14
-        question: "",
-        answers: ["","","",""]
+        question: "What keyword is used to get the a variable's datatype?",
+        answers: ["typeof","datatype","value","content"]
     },
     { // #15
-        question: "",
-        answers: ["","","",""]
+        question: "Name this symbol: !",
+        answers: ["bang operator","exclamation point","opposite","important"]
     },
     { // #16
-        question: "",
-        answers: ["","","",""]
+        question: "In an object, each property assignment ends with what symbol?",
+        answers: [";",",",":",")"]
     },
     { // #17
-        question: "",
-        answers: ["","","",""]
+        question: "What is the HTML element used to connect a javascript file",
+        answers: ["<script>","<link>","<source>","<javascript>"]
     },
     { // #18
-        question: "",
-        answers: ["","","",""]
+        question: "What is the operator to assign a variable it's value?",
+        answers: ["=","===","==",":"]
     },
     { // #19
-        question: "",
-        answers: ["","","",""]
+        question: "What is the operator used to compare the value and type of two variables?",
+        answers: ["===","==","=",":"]
     },
     { // #20
-        question: "",
-        answers: ["","","",""]
+        question: "What is the operator used to compare only the value of two variables?",
+        answers: ["==","=","===",":"]
     },
     { // #21
-        question: "",
-        answers: ["","","",""]
+        question: "What kind of error occurs when a loop cannot escape itself?",
+        answers: ["runtime","syntax","logic","compiling"]
     },
     { // #22
-        question: "",
-        answers: ["","","",""]
+        question: "What is the keyword used to refer to the entirety of a webpage?",
+        answers: ["window","page","webpage","screen"]
     },
     { // #23
-        question: "",
-        answers: ["","","",""]
+        question: "What is the structure used for selection?",
+        answers: ["if statement","for loop","while loop","sequencing"]
     },
     { // #24
-        question: "",
-        answers: ["","","",""]
+        question: "var num = parseInt('four'); \n What value does the variable hold?",
+        answers: ["NaN","four","4","undefined"]
     },
     { // #25
-        question: "",
-        answers: ["","","",""]
+        question: "var num = 'four'; \n What value does the variable hold?",
+        answers: ["four","4","NaN","undefined"]
     }
     ]
 }
